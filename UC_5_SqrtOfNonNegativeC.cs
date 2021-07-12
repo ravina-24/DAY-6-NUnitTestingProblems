196a7afe23f1d63032eccbaa8b0105ec228a6fed
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-12 23:48:35 +0530

    [Ravina] Add: Finding the square root of non negative number C

---
+++ UC_5_SqrtOfNonNegativeC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC_5_SqrtOfNewtons_sMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the value of c  :   ");
+            double c = double.Parse(Console.ReadLine());
+
+
+
+
+            double t;
+           
+          
+            t = c;
+
+            double epsilon = 1e-15;
+
+            while (Math.Abs(t - c / t) > epsilon * t)
+            { 
+
+                    t = (c / t + t)/2.0 ;
+            }
+
+            Console.WriteLine("The  value of t is : " + t);
+
+            Console.ReadLine();
+        }
+    }
+}

5ab07151d481fd2b2e1232433c1455724d664ffa
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-12 23:33:59 +0530

    [Ravina] Add:Finding  the minimum number of number of notes as well as notes notes to be returned by the vending machine

---
+++ UC_1_VendingMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UC_1_VendingMahine
+{
+    class UC_1_VendingMachine
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("......Welcome to Vending Machine Application.......");
+
+            int Rupees;
+            Console.Write("Enter the rupees  :   ");
+            Rupees = int.Parse(Console.ReadLine());
+
+
+            int i = 0;
+
+
+            int[] Notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+            int[] TotalNotes = new int [] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+            for (i = 0; i < 8; i++)
+            {
+                if (Rupees >=Notes[i])
+                {
+                    Notes[i] = Rupees / Notes[i];
+                    Rupees = Rupees - TotalNotes[i] * Notes[i];
+
+                    Console.WriteLine("Total  notes should be return    :" + Notes[i] + ":" + TotalNotes[i]);
+
+               
+                   
+                }
+              
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}

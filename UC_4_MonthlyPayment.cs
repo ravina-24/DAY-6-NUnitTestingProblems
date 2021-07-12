6efb62b407a84504b9714b2ca019a8dda865fc68
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-12 23:45:09 +0530

    [Ravina] Add: Calculating the monthly Payment

---
+++ UC_4_MonthlyPayment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UC_4_MonthlyPayment
+{
+    class UC_4_MonthlyPayment
+    {
+        static void Main(string[] args)
+        {
+
+            double P; //principal
+
+            Console.Write("Enter the Principal :   ");
+            P = double.Parse(Console.ReadLine());
+
+
+            double Y;  // year 
+
+            Console.Write("Enter the number of year :  ");
+            Y = double.Parse(Console.ReadLine());
+
+            double R; // rate 
+            Console.Write("Enter the Rate: ");
+            R = double.Parse(Console.ReadLine());
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double Monthlypayment = (P * r*Math.Pow(1+r, n)) /(Math.Pow(1 + r ,n)-1);
+
+            Console.WriteLine("Monthly payments is   : " + Monthlypayment);
+            Console.ReadLine();
+
+
+        }
+    }
+}

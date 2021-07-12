0db6a58b9c74af1c9eb2402ca0e3a6a6dabf72a6
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-12 23:51:31 +0530

    [Ravina] Add: Conversion of decimal number into binary form

---
+++ UC_6_toBinary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UC_6_ToBinaryCoversion
+{
+    class UC_6_toBinary
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Console.Write("Enter the Decimal Number : ");
+            Num = int.Parse(Console.ReadLine());
+
+            int[] BinaryArray = new int[32];
+
+            int i = 0;
+            while (Num>0)
+            {
+                BinaryArray[i] = Num % 2;
+                Num = Num / 2;
+                i++;
+
+
+            }
+            int j = 0;
+            for (j = i - 1; j >= 0; j--)
+            {
+                Console.WriteLine("Binary Conversion :" + BinaryArray[j]);
+            }
+
+            Console.ReadLine();
+
+
+
+
+             }
+           
+        }
+      
+    }
+
